a5e483d8723accafce7f018eb4585bc70ed6d6db
Luca Hendrik Helms <github@lucahelms.dev>
2024-02-06 23:16:38 +0100

    add router & window api example

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,24 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { RouterLink, RouterView } from "vue-router";
+import { WebviewWindow } from "@tauri-apps/api/window";
+
+function handleClick() {
+  const label = crypto.randomUUID();
+  console.log(label);
+  const webview = new WebviewWindow(label, {
+    url: "/about",
+  });
+  webview.show();
+}
+</script>
 
 <template>
   <div class="container">
     <h1>Welcome to Tauri!</h1>
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/about">About</RouterLink>
+    <button @click="handleClick">Open about window</button>
   </div>
+
+  <RouterView />
 </template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const counter = ref(0);
+</script>
+
+<template>
+  <h1>Home</h1>
+  <button @click="() => counter++">Increment</button>
+  <p>{{ counter }}</p>
+</template>
